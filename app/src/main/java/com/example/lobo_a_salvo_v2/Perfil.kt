677578ca487bf0474520b7550631f66c1564cfff
Raidnor <hueyogarciamarcelino@outlook.com>
2023-05-30 22:01:21 -0600

    Actualizacion:prototipo v3

--- app/src/main/java/com/example/lobo_a_salvo_v2/Perfil.kt
+++ app/src/main/java/com/example/lobo_a_salvo_v2/Perfil.kt
@@ -2,9 +2,12 @@ package com.example.lobo_a_salvo_v2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 
 class Perfil : AppCompatActivity() {
+    private lateinit var foto_perfil:ImageView
     private lateinit var correo_text:TextView
     private var correo = MainActivity.data.correo
 
@@ -14,5 +17,10 @@ class Perfil : AppCompatActivity() {
 
         correo_text = findViewById<TextView>(R.id.correo_set)
         correo_text.setText(correo)
+        foto_perfil = findViewById<ImageView>(R.id.imageView5)
+        Glide.with(this)
+            .load(R.drawable.bremerton_ship)
+            .circleCrop()
+            .into(foto_perfil)
     }
 }
